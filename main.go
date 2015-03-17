29a320a55f53ef3474b5aa1e3f99f46cfd9373f1
Brad Olson <iam@brad.io>
2015-03-17 08:51:35 -0700

    foobar

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ type slack struct{}
 
 func (s slack) Push(e *webhook.PushEvent) {
 	const format = "https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s"
-	text := url.QueryEscape(fmt.Sprintf("test"))
+	text := url.QueryEscape(fmt.Sprintf("%s pushed to %s", e.Pusher.Email, e.Repository.Name))
+	fmt.Println(e)
 
-	fmt.Println(text)
 	if _, err := http.Get(fmt.Sprintf(format, *token, *channel, text)); err != nil {
 		log.Println(err)
 	}
