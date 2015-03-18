a9737424cd2c8f78bb13eb484071ab92654fb902
Brad Olson <iam@brad.io>
2015-03-17 19:53:27 -0700

    add pushevent. queryURL

--- main.go
+++ main.go
@@ -14,23 +14,35 @@ var (
 	secret  = flag.String("secret", "", "GitHub webhook secret")
 	token   = flag.String("token", "", "Slack API token")
 	channel = flag.String("channel", "", "Slack channel name")
+	base    = "https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s"
 )
 
 type slack struct{}
 
-func (s slack) Push(e *webhook.PushEvent) {
-	const format = "https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s"
-	text := url.QueryEscape(fmt.Sprintf("%s pushed to %s", e.Pusher.Email, e.Repository.Name))
-	fmt.Println(e.Repository.URL)
+func (s slack) queryURL(message string) string {
+	escaped := url.QueryEscape(message)
+	return fmt.Sprintf(base, *token, *channel, escaped)
+}
 
-	if _, err := http.Get(fmt.Sprintf(format, *token, *channel, text)); err != nil {
+func (s slack) Push(e *webhook.PushEvent) {
+	message := fmt.Sprintf("%s pushed to <%s|%s>", e.Pusher.Email, e.Repository.URL, e.Repository.Name)
+
+	_, err := http.Get(s.queryURL(message))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (s slack) PullRequest(e *webhook.PullRequestEvent) {
+	message := fmt.Sprintf("new pull request at %s", e.PullRequest.URL)
+
+	_, err := http.Get(s.queryURL(message))
+	if err != nil {
 		log.Println(err)
 	}
 }
 
 func main() {
 	flag.Parse()
-	fmt.Println("Secret: ", *secret, "Token:", *token, "Channel:", *channel)
-
 	log.Fatal(http.ListenAndServe(":8080", webhook.New(*secret, slack{})))
 }
